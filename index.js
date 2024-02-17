0f573c78433fcf39e2005ef389b2df456f136a04
José García <jose.garcia26@utp.ac.pa>
2024-02-16 23:51:03 -0500

    javascript, icon spotify, mp3

---
+++ index.js
@@ -0,0 +1,36 @@
+let divBaner2 = document.getElementById("baner");
+divBaner2.addEventListener("mouseover", hoverIn);
+divBaner2.addEventListener("mouseout", hoverOut);
+divBaner2.addEventListener("click", clickHandler);
+
+let player = document.getElementById("player");
+
+
+function hoverIn() {
+  console.log("entraste");
+  player.autoplay = true;
+  player.play();
+  divBaner2.style.animationPlayState = "running";
+  return true;
+}
+
+function hoverOut() {
+  console.log("saliste");
+  if (!musicState) {
+    player.pause();
+    divBaner2.style.animationPlayState = "paused";
+  }
+}
+
+function clickHandler(e) {
+  console.log("Se hizo clic en spotify");
+  if (!musicState) {
+    musicState = true;
+    divBaner2.style.animationPlayState = "running";
+  } else {
+    musicState = false;
+    divBaner2.style.animationPlayState = "paused";
+  }
+  console.log(musicState);
+}
+let musicState = false;
